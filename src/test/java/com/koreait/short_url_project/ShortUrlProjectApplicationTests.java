ecf40deca5800e0f9e9597fec31b32704a072a70
guri670 <kory0485@gmail.com>
2025-02-12 16:45:44 +0900

    shortUrl

---
+++ src/test/java/com/koreait/short_url_project/ShortUrlProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreait.short_url_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortUrlProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
